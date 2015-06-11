19359b92253b2390cc3285a3d03a99752226db45
Pavan Mandava <mspavan04@gmail.com>
2015-06-11 18:03:47 +0530

    Initial Commit of Primary Key Generators on 11th June 2015

---
+++ NativeGeneratorPostgreSQL/src/com/hibernate/client/ClientApp.java
@@ -0,0 +1,31 @@
+package com.hibernate.client;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.pojo.Employee;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		Configuration configuration = new Configuration();
+		configuration.configure("com/hibernate/config/hibernate.cfg.xml");
+		SessionFactory factory = configuration.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction transaction = session.beginTransaction();
+
+		Employee employee = new Employee();
+		//employee.setEmployeeId(88);
+		employee.setEmployeeName("Gregg");
+		employee.setSalary(95000);
+
+		Object employee2 = session.save(employee);
+		transaction.commit();
+
+		System.out.println("Employee Id : " + employee2);
+
+	}
+
+}
